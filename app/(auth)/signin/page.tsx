d4dff8335e8218ce09b9609e1fd814634362d790
Asteris Giannoudis <asteris.gian@gmail.com>
2023-07-14 17:41:14 +0300

    Changed Sign in button. Successful login redirects to home page.

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -28,7 +28,7 @@ export default function SignIn() {
         },
       }).then((response) => {
         if (response.status === 200) {
-          router.push("/protected");
+          router.push("/");
         }
       });
     });
@@ -108,46 +108,21 @@ export default function SignIn() {
           <div className="flex flex-wrap">
             <div className="w-full">
               <button
-                className="btn-sm text-white bg-[#808080] w-full relative flex items-center"
                 type="button"
                 onClick={signIn}
+                className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
               >
                 <svg
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="mr-2"
+                  viewBox="0 0 1792 1792"
                   xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="13"
-                  viewBox="0 0 186.69 190.5"
                 >
-                  <g transform="translate(1184.583 765.171)">
-                    <path
-                      clipPath="none"
-                      mask="none"
-                      d="M-1089.333-687.239v36.888h51.262c-2.251 11.863-9.006 21.908-19.137 28.662l30.913 23.986c18.011-16.625 28.402-41.044 28.402-70.052 0-6.754-.606-13.249-1.732-19.483z"
-                      fill="#4285f4"
-                    />
-                    <path
-                      clipPath="none"
-                      mask="none"
-                      d="M-1142.714-651.791l-6.972 5.337-24.679 19.223h0c15.673 31.086 47.796 52.561 85.03 52.561 25.717 0 47.278-8.486 63.038-23.033l-30.913-23.986c-8.486 5.715-19.31 9.179-32.125 9.179-24.765 0-45.806-16.712-53.34-39.226z"
-                      fill="#34a853"
-                    />
-                    <path
-                      clipPath="none"
-                      mask="none"
-                      d="M-1174.365-712.61c-6.494 12.815-10.217 27.276-10.217 42.689s3.723 29.874 10.217 42.689c0 .086 31.693-24.592 31.693-24.592-1.905-5.715-3.031-11.776-3.031-18.098s1.126-12.383 3.031-18.098z"
-                      fill="#fbbc05"
-                    />
-                    <path
-                      d="M-1089.333-727.244c14.028 0 26.497 4.849 36.455 14.201l27.276-27.276c-16.539-15.413-38.013-24.852-63.731-24.852-37.234 0-69.359 21.388-85.032 52.561l31.692 24.592c7.533-22.514 28.575-39.226 53.34-39.226z"
-                      fill="#ea4335"
-                      clipPath="none"
-                      mask="none"
-                    />
-                  </g>
+                  <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
                 </svg>
-                <span className="flex-auto pl-16 pr-8 -ml-16">
-                  Sign in with Google
-                </span>
+                Sign in with Google
               </button>
             </div>
           </div>
