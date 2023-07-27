e427e9188b4a234b277445752243e0ee008a16e9
Asteris Giannoudis <asteris.gian@gmail.com>
2023-07-27 13:42:53 +0300

    Panel contains 2 working and one non-working buttons.

--- components/ui/side-navigation.tsx
+++ components/ui/side-navigation.tsx
@@ -32,10 +32,7 @@ export default function SideNavigation() {
                 <Link
                   href="/"
                   className={`w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 ${
-                    pathname !== "/about" &&
-                    pathname !== "/subscribe" &&
-                    pathname !== "/projects" &&
-                    pathname !== "/resume"
+                    pathname === "/"
                       ? "text-sky-500 after:bg-sky-500"
                       : "text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400"
                   }`}
@@ -76,7 +73,7 @@ export default function SideNavigation() {
                   </svg>
                 </Link>
               </li>
-              <li className="py-2">
+              {/* <li className="py-2">
                 <Link
                   href="/projects"
                   className={`w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 ${
@@ -96,17 +93,17 @@ export default function SideNavigation() {
                     <path d="M8 3h4V2H8v1ZM6 3V0h8v3h6v12H0V3h6ZM2 5v8h16V5H2Zm14 13v-2h2v4H2v-4h2v2h12Z" />
                   </svg>
                 </Link>
-              </li>
+              </li> */}
               <li className="py-2">
                 <Link
-                  href="/resume"
+                  href="/blog"
                   className={`w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 ${
-                    pathname === "/resume"
+                    pathname.includes("/blog")
                       ? "text-sky-500 after:bg-sky-500"
                       : "text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400"
                   }`}
                 >
-                  <span className="sr-only">Resume</span>
+                  <span className="sr-only">Blog</span>
                   <svg
                     className="fill-current"
                     xmlns="http://www.w3.org/2000/svg"
@@ -125,7 +122,7 @@ export default function SideNavigation() {
                   </svg>
                 </Link>
               </li>
-              <li className="py-2">
+              {/* <li className="py-2">
                 <Link
                   href="/subscribe"
                   className={`w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 ${
@@ -145,7 +142,7 @@ export default function SideNavigation() {
                     <path d="M13.331 15.169 17.37 3.63 5.831 7.669l5.337 2.163 2.163 5.337Zm-3.699-3.801L.17 7.53 20.63.37l-7.161 20.461-3.837-9.463Z" />
                   </svg>
                 </Link>
-              </li>
+              </li> */}
             </ul>
           </nav>
         </div>
