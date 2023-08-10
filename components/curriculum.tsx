0177d3d5bf0426116670c52582ff827f8ec78623
Kostas Minaidis <minaidisk@gmail.com>
2023-08-10 12:35:01 +0300

    Updated Curriculum Component: fixed typos, removed bold fonts, updated GitHub link

--- components/curriculum.tsx
+++ components/curriculum.tsx
@@ -18,7 +18,7 @@ export default function Curriculum() {
               <h3 className="h3 font-cabinet-grotesk text-4xl mb-8">
                 WDX 180°
               </h3>
-              <p className="text-xl font-bold">
+              <p className="text-xl">
                 WDX 180° is a 9-month intensive course designed to teach you
                 software engineering and web development, all the way through, from
                 the basics  of computing and the World Wide Web to mastering
@@ -41,18 +41,14 @@ export default function Curriculum() {
                 <h3 className="h3 font-cabinet-grotesk text-4xl mb-4">
                   Beyond the basics.
                 </h3>
-                <p className="text-xl mb-6 font-bold">
-                  intechgration's 9-month-long program goes beyond just the
-                  introcutions. We take a deep, project-based, dive
-                  into every tool and the underlying CS concepts to make sure
-                  our curious studetns, no matter the background, truly become competent
-                  engineers.
+                <p className="text-xl mb-6">
+                  in<span className="font-bold">tech</span>gration's 9-month-long program goes beyond just the introductions. We take a deep, project-based dive into every tool and the underlying CS concepts to make sure our curious students, no matter the background, truly become competent engineers.
                 </p>
                 <div>
                   {/* TODO provide wdx180 github link */}
                   <Link
                     className="btn font-bold text-white bg-black hover:bg-gray-800 shadow-md hover:shadow-lg"
-                    href="/#"
+                    href="https://github.com/in-tech-gration/WDX-180"
                   >
                     <svg
                       className="w-8 h-8 fill-current"
@@ -61,7 +57,7 @@ export default function Curriculum() {
                     >
                       <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
                     </svg>{" "}
-                    WDX 180° githuh repo
+                    WDX 180° GitHub repo
                   </Link>
                 </div>
               </div>
