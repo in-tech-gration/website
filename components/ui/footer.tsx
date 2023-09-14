501c09f6ca7df1c77a92afd2e969fd1d02b66a95
Kostas Minaidis <minaidisk@gmail.com>
2023-09-14 18:10:52 +0300

    Integrating Olark chat client

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Script from 'next/script'
 
 export default function Footer() {
   return (
@@ -90,6 +91,19 @@ export default function Footer() {
           </div>
         </div>
       </div>
+      <Script id="olark-integration">
+          {`
+            ;(function(o,l,a,r,k,y){if(o.olark)return;
+              r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+              y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+              y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+              y.extend=function(i,j){y("extend",i,j)};
+              y.identify=function(i){y("identify",k.i=i)};
+              y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+              k=y._={s:[],t:[+new Date],c:{},l:a};
+              })(window,document,"static.olark.com/jsclient/loader.js");/* custom configuration goes here (www.olark.com/documentation) */olark.identify('8678-910-10-4538');
+          `}
+      </Script>
     </footer>
   );
 }
