d7f498f18796d9213f36dfdb0536144d863aec3f
Kostas Minaidis <minaidisk@gmail.com>
2024-01-02 13:53:00 +0200

    [ CERTIFICATE ] Added Generic type on useParams id

--- pages/certificate/[id].tsx
+++ pages/certificate/[id].tsx
@@ -1,13 +1,19 @@
 import Certificate from "@/components/certificate"
 import { useParams } from 'next/navigation'
 
+type Params = {
+  id: string
+}
+
 const Cert = () => {
-  const params = useParams()
-  console.log(params)
-  if (params){
-    // @ts-ignore
-    return <Certificate id={params?.id}/>
+
+  const params = useParams<Params>();
+  const hasIdParam = params && params.id;
+
+  if ( hasIdParam ){
+    return <Certificate id={params.id}/>
   }
-  }
+
+}
   
 export default Cert;
