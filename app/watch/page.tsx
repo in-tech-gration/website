982b9f2c0c52f921dff00c5e2e9f16b612ed298d
Kostas Minaidis <minaidisk@gmail.com>
2024-08-25 18:01:11 +0300

    Revert

--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -260,10 +260,3 @@ Watch.getLayout = function getLayout(page) {
   )
 }
 
-export async function generateMetadata({ params }: any) {
- 
-  return {
-    title: "A Hacker's Playlist - intechgration.io",
-    description: "Films, series and documentaries for hackers. ;)",
-  };
-}
